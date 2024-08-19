bdd121443cb88aabbf194c8696b42e91d6983148
Kynes <kamilpirog98@gmail.com>
2024-08-19 11:20:45 +0200

    feat: handle bots queue

--- Assets/MenuUI.cs
+++ Assets/MenuUI.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 public class MenuUI : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button trainingButton;
 
     private void Awake()
     {
         playButton.onClick.AddListener(OnClick);
+        trainingButton.onClick.AddListener(OnTrainingButtonClicked);
     }
 
     private void OnClick()
     {
-        ElympicsLobbyClient.Instance.PlayOnlineInRegion("warsaw");
+        ElympicsLobbyClient.Instance.PlayOnlineInRegion("warsaw", null, null, "Default");
+    }
+
+    private void OnTrainingButtonClicked()
+    {
+        ElympicsLobbyClient.Instance.PlayOnlineInRegion("warsaw", null, null, "bots");
     }
 }
